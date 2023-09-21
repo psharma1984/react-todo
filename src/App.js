16df4dcc9d8ca15b013cb0d34baa7a742c565682
psharma1984 <psharma.cad@gmail.com>
2023-09-20 17:22:53 -0700

    created a list component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-
+import * as React from 'react';
+const todoList = [
+  {
+    id:1,
+    title:"First assignment",
+  },
+  {
+    id:2,
+    title:"Second assignment",
+  },
+  {
+    id:3,
+    title:"Third assignment",
+  },
+]
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'left' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>ToDo List</h1>
+        <ul>
+          {todoList.map(function(item) {
+            return<li key={item.id}>{item.title}</li>;            
+          })}
+        </ul>       
       </header>
     </div>
   );
