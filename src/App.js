74ebb70d721ff7f66ee13a945349a969c89a974e
psharma1984 <psharma.cad@gmail.com>
2023-09-30 19:09:46 -0700

    created TodoForm and TodoList component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import * as React from 'react';
-const todoList = [
-  {
-    id:1,
-    title:"First assignment",
-  },
-  {
-    id:2,
-    title:"Second assignment",
-  },
-  {
-    id:3,
-    title:"Third assignment",
-  },
-]
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
+
 function App() {
   return (
     <div style={{ textAlign: 'left' }}>
       <header>
-        <h1>ToDo List</h1>
-        <ul>
-          {todoList.map(function(item) {
-            return<li key={item.id}>{item.title}</li>;            
-          })}
-        </ul>       
+        <h1>ToDo List</h1> 
+        <TodoList />  
+        <AddTodoForm />         
       </header>
     </div>
   );
