e203d7a8e3d587036d427f219b7243a1188ff236
psharma1984 <psharma.cad@gmail.com>
2023-11-04 23:48:47 -0700

    answer lesson_6

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-  {
-    id: 1,
-    title: "First assignment",
-  },
-  {
-    id: 2,
-    title: "Second assignment",
-  },
-  {
-    id: 3,
-    title: "Third assignment",
-  },
-]
-function TodoList({ todoList }) {
+// const todoList = [
+//   {
+//     id: 1,
+//     title: "First assignment",
+//   },
+//   {
+//     id: 2,
+//     title: "Second assignment",
+//   },
+//   {
+//     id: 3,
+//     title: "Third assignment",
+//   },
+// ]
+function TodoList({ todoList, onRemoveTodo }) {
   return (
     <ul>
       {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
+        <TodoListItem key={todo.id} todo={todo} onRemoveTodo={onRemoveTodo} />
       ))}
     </ul>
   );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function TodoListItem({ todo }) {
-    return (<li >{todo.title}</li>);
+function TodoListItem({ todo, onRemoveTodo }) {
+    function removeFunc() {
+        onRemoveTodo(todo.id)
+    }
+    return (<li >{todo.title}  <button type='button' onClick={removeFunc}>Remove</button>
+    </li>);
 }
 
 export default TodoListItem;
