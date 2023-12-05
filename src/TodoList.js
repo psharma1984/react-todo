3c56d221f9271036d7144f377cc6b98e2fbfc6e8
psharma1984 <psharma.cad@gmail.com>
2023-12-04 21:10:02 -0800

    removed hard coded todolist from todolist.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-  {
-    id: 1,
-    title: "First assignment",
-  },
-  {
-    id: 2,
-    title: "Second assignment",
-  },
-  {
-    id: 3,
-    title: "Third assignment",
-  },
-]
 function TodoList({ todoList }) {
   return (
     <ul>
